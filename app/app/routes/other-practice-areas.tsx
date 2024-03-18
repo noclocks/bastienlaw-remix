5b0d50363c31b29f9bf68579bb8e43e285af686f
Patrick Howard <patrick.howard@noclocks.dev>
2024-03-18 18:48:31 -0400

    `OtherPracticeAreas` page & stylesheet

---
+++ app/app/routes/other-practice-areas.tsx
@@ -0,0 +1,164 @@
+import {Footer} from "../components/Footer";
+import {Header} from "../components/Header";
+
+import "../styles/other-practice-areas.css"
+
+export default function OtherPracticeAreas() {
+  return (
+    <div id="page-container">
+        <div id="et-boc" className="et-boc">
+            <Header />
+            <div id="et-main-area">
+                <div id="main-content">
+                    <div className="et-l et-l--body">
+                        <div className="et_builder_inner_content et_pb_gutters1">
+                            <div className="et_pb_section et_pb_section_1_tb_body et_pb_with_background et_section_regular">
+                                <div className="et_pb_row et_pb_row_0_tb_body">
+                                    <div
+                                        className="et_pb_column et_pb_column_4_4 et_pb_column_0_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child">
+                                        <div
+                                            className="et_pb_module et_pb_text et_pb_text_0_tb_body  et_pb_text_align_center et_pb_bg_layout_light">
+                                            <div className="et_pb_text_inner">
+                                                <p>Dedication. Skill. Innovation.<br/> Trust The <strong><span
+                                                            className="nap-item nap-item--name">Law Offices of Villard
+                                                            Bastien, LLC</span></strong>.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                className="et_pb_section et_pb_section_3_tb_body fl-breadcrumbs et_pb_with_background et_section_regular">
+                                <div className="et_pb_row et_pb_row_1_tb_body">
+                                    <div
+                                        className="et_pb_column et_pb_column_4_4 et_pb_column_1_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child">
+                                        <div
+                                            className="et_pb_module et_pb_dcsbcm_divi_breadcrumbs_module et_pb_dcsbcm_divi_breadcrumbs_module_0_tb_body">
+                                            <div className="et_pb_module_inner">
+                                                <ol className="dcsbcm_divi_breadcrumbs"
+                                                  // vocab="http://schema.org/"
+                                                  // typeof="BreadcrumbList"
+                                                >
+                                                    <li className="dcsbcm_divi_breadcrumb"
+                                                      // property="itemListElement"
+                                                      // typeof="ListItem"
+                                                    >
+                                                      <a
+                                                        // property="item"
+                                                        // typeof="WebPage"
+                                                        href="../index.htm">
+                                                          <span
+                                                            // property="name"
+                                                          >
+                                                            Home
+                                                          </span>
+                                                      </a>
+                                                        <meta property="position" content="1"/>
+                                                    </li>
+                                                    <span className="dcsbcm_separator">&nbsp;&raquo;&nbsp;</span>
+                                                    <span
+                                                      className="dcsbcm_divi_breadcrumb dcsbcm_divi_breadcrumb-active">
+                                                        Other Practice Areas
+                                                    </span>
+                                                </ol>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="et_pb_section et_pb_section_4_tb_body et_pb_with_background et_section_regular">
+                                <div className="et_pb_row et_pb_row_2_tb_body">
+                                    <div
+                                        className="et_pb_column et_pb_column_2_3 et_pb_column_2_tb_body fl-main-content  et_pb_css_mix_blend_mode_passthrough">
+                                        <div className="et_pb_module et_pb_post_content et_pb_post_content_0_tb_body">
+                                            <h1>How My Firm Can Support Your Other Legal Needs</h1>
+                                            <p>Every time a person walks through my door at the <span
+                                                    className="nap-item nap-item--name">Law Offices of Villard Bastien,
+                                                    LLC</span>, with a legal problem, there is a different set of
+                                                challenges and struggles to address. You are not like anyone else, and
+                                                you deserve to work with an attorney who understands that. Moreover, you
+                                                deserve to work with an attorney who has prepared for a wide variety of
+                                                potential issues.</p>
+                                            <p>I serve clients across Georgia and am licensed to practice in New York
+                                                and New Jersey. You can call me directly by dialing <span
+                                                    className="nap-item nap-item--phone"><a href="tel:+1-404-865-1952"
+                                                        data-wpel-link="internal">404-865-1952</a></span> or you can <a
+                                                    href="../contact/index.htm" data-wpel-link="internal">reach me
+                                                    online</a> to talk about your experiences and get solutions that
+                                                work for you.</p>
+                                            <h2>Supporting You When The Unexpected Happens</h2>
+                                            <p>My extensive legal history and decades working with clients prepare me
+                                                for almost any issue you may have with the law. In addition to <a
+                                                    href="../nursing-home-abuse/index.htm" data-wpel-link="internal">my
+                                                    primary practice area</a>, I can help you navigate:</p>
+                                            <ul>
+                                                <li><a href="../serious-injury/index.htm"
+                                                        data-wpel-link="internal">Slip-and-fall accidents</a></li>
+                                                <li><a href="../serious-injury/index.htm"
+                                                        data-wpel-link="internal">Wrongful death claims</a></li>
+                                                <li><a href="../serious-injury/index.htm"
+                                                        data-wpel-link="internal">Medical malpractice suits</a></li>
+                                                <li><a href="../criminal-law-defense/index.htm"
+                                                        data-wpel-link="internal">Criminal defense</a></li>
+                                            </ul>
+                                            <p>You’ll find a kind ear at the <span className="nap-item nap-item--name">Law
+                                                    Offices of Villard Bastien, LLC</span>, to guide you through
+                                                whatever legal steps are needed to protect you and your loved ones.</p>
+                                            <h2>You Can Trust Me To Be Your Strongest Defender</h2>
+                                            <p>I am a seasoned advocate who has been through this before. I will tailor
+                                                a strategic case that will give you the best opportunity for success.
+                                                These can include timely filings, challenging evidence, scrutinizing
+                                                procedures or fighting illegal searches. I listen with compassion – then
+                                                propose a comprehensive plan to move forward.</p>
+                                            <p>Do not wait. Call me now at <span className="nap-item nap-item--phone"><a
+                                                        href="tel:+1-404-865-1952"
+                                                        data-wpel-link="internal">404-865-1952</a></span> for a free
+                                                consultation. I speak English, Spanish and French.</p>
+                                        </div>
+                                    </div>
+                                    <div
+                                        className="et_pb_column et_pb_column_1_3 et_pb_column_3_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child">
+                                        <div id="nav-practice-silo"
+                                            className="et_pb_module et_pb_sidebar_0_tb_body nav-practice-silo et_pb_widget_area clearfix et_pb_widget_area_left et_pb_bg_layout_light">
+                                            <div id="nav_menu-2" className="et_pb_widget widget_nav_menu">
+                                                <h2 className="widgettitle">Practice Areas</h2>
+                                                <div className="menu-practice-areas-container">
+                                                    <ul id="menu-practice-areas" className="menu">
+                                                        <li className="menu-item menu-item--depth-0"><a
+                                                                href="../nursing-home-abuse/index.htm" tabIndex={0}
+                                                                data-wpel-link="internal">Nursing Home Abuse</a></li>
+                                                        <li
+                                                            className="menu-item menu-item-has-children current-menu-item menu-item--depth-0">
+                                                            <a href="index.htm" aria-current="page" tabIndex={0}
+                                                                data-wpel-link="internal">Other Practice Areas</a>
+                                                            <ul className="sub-menu">
+                                                                <li className="menu-item menu-item--depth-1"><a
+                                                                        href="../business-general-counsel-business-transactions/index.htm"
+                                                                        tabIndex={0} data-wpel-link="internal">Business
+                                                                        General Counsel/ Business Transactions</a></li>
+                                                                <li className="menu-item menu-item--depth-1"><a
+                                                                        href="../criminal-law-defense/index.htm"
+                                                                        tabIndex={0} data-wpel-link="internal">Criminal
+                                                                        Law Defense</a></li>
+                                                                <li className="menu-item menu-item--depth-1"><a
+                                                                        href="../serious-injury/index.htm" tabIndex={0}
+                                                                        data-wpel-link="internal">Serious Injury</a>
+                                                                </li>
+                                                            </ul>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        </div>
+    </div>
+  )
+}
