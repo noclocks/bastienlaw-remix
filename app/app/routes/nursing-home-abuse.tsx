c9020af151c1600257df84822a29b7fb6258610e
Patrick Howard <patrick.howard@noclocks.dev>
2024-03-18 18:09:20 -0400

    `NursingHomeAbuse` w/ corresponding stylesheet

---
+++ app/app/routes/nursing-home-abuse.tsx
@@ -0,0 +1,255 @@
+import {Footer} from "../components/Footer";
+import {Header} from "../components/Header";
+
+import "../styles/nursing-home-abuse.css";
+
+export default function NursingHomeAbuse() {
+  return (
+    <div id="page-container">
+      <div id="et-boc" className="et-boc">
+        <Header />
+        <div id="et-main-area">
+          <div id="main-content">
+            <div className="et-l et-l--body">
+              <div className="et_builder_inner_content et_pb_gutters1">
+                <div className="et_pb_section et_pb_section_1_tb_body et_pb_with_background et_section_regular">
+                  <div className="et_pb_row et_pb_row_0_tb_body">
+                    <div className="et_pb_column et_pb_column_4_4 et_pb_column_0_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child">
+                      <div className="et_pb_module et_pb_text et_pb_text_0_tb_body  et_pb_text_align_center et_pb_bg_layout_light">
+                        <div className="et_pb_text_inner">
+                          <p>
+                            Dedication. Skill. Innovation.
+                            <br /> Trust The{" "}
+                            <strong>
+                              <span className="nap-item nap-item--name">
+                                Law Offices of Villard Bastien, LLC
+                              </span>
+                            </strong>
+                            .
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="et_pb_section et_pb_section_3_tb_body fl-breadcrumbs et_pb_with_background et_section_regular">
+                  <div className="et_pb_row et_pb_row_1_tb_body">
+                    <div className="et_pb_column et_pb_column_4_4 et_pb_column_1_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child">
+                      <div className="et_pb_module et_pb_dcsbcm_divi_breadcrumbs_module et_pb_dcsbcm_divi_breadcrumbs_module_0_tb_body">
+                        <div className="et_pb_module_inner">
+                          <ol
+                            className="dcsbcm_divi_breadcrumbs"
+                            // vocab="http://schema.org/"
+                            // typeof="BreadcrumbList"
+                          >
+                            <li
+                              className="dcsbcm_divi_breadcrumb"
+                              // property="itemListElement"
+                              // typeof="ListItem"
+                            >
+                              <a
+                                // property="item"
+                                // typeof="WebPage"
+                                href="../index.htm"
+                                data-wpel-link="internal"
+                              >
+                                <span
+                                // property="name"
+                                >
+                                  Home
+                                </span>
+                              </a>
+                              <meta property="position" content="1" />
+                            </li>
+                            <span className="dcsbcm_separator">
+                              &nbsp;&raquo;&nbsp;
+                            </span>
+                            <span className="dcsbcm_divi_breadcrumb dcsbcm_divi_breadcrumb-active">
+                              Nursing Home Abuse
+                            </span>
+                          </ol>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="et_pb_section et_pb_section_4_tb_body et_pb_with_background et_section_regular">
+                  <div className="et_pb_row et_pb_row_2_tb_body">
+                    <div className="et_pb_column et_pb_column_2_3 et_pb_column_2_tb_body fl-main-content  et_pb_css_mix_blend_mode_passthrough">
+                      <div className="et_pb_module et_pb_post_content et_pb_post_content_0_tb_body">
+                        <h1>
+                          There Is A Nursing Home Abuse And Neglect Epidemic
+                        </h1>
+                        <p>
+                          As older loved ones need more and more care, most
+                          people must make a difficult decision: How do you
+                          adequately care for your loved one&rsquo;s increasing
+                          needs while still providing for yourself and living
+                          your life? For many, the answer is placing your older
+                          family members in the care of a nursing facility.
+                        </p>
+                        <p>
+                          Unfortunately, sometimes those facilities do not
+                          follow standards of care, and our elderly loved ones
+                          are harmed as a result.
+                        </p>
+                        <p>
+                          Feel free to call my firm, the{" "}
+                          <span className="nap-item nap-item--name">
+                            Law Offices of Villard Bastien, LLC
+                          </span>
+                          , at{" "}
+                          <span className="nap-item nap-item--phone">
+                            <a
+                              href="tel:+1-404-865-1952"
+                              data-wpel-link="internal"
+                            >
+                              404-865-1952
+                            </a>
+                          </span>{" "}
+                          and talk to me about how I can help you and your
+                          family in the wake of nursing home abuse and neglect.
+                        </p>
+                        <h2>
+                          Are You Concerned About The Quality Of Care Your Loved
+                          One Is Getting?
+                        </h2>
+                        <p>
+                          Unfortunately, even the most well-intentioned nursing
+                          facility is often over-burdened and understaffed. This
+                          often results in medical errors and opportunities for
+                          abuse. Experiencing concern is normal. You should call
+                          an attorney if you see consistent patterns of abuse or
+                          neglect, including:
+                        </p>
+                        <ul>
+                          <li>Recurrent bed sores or ulcers</li>
+                          <li>
+                            Unaddressed issues with medication, including
+                            misdosing of medications and unnecessary treatment
+                          </li>
+                          <li>Unexplained bruising or injuries</li>
+                          <li>
+                            Missing valuables or discrepancies with bills and
+                            bank accounts
+                          </li>
+                          <li>
+                            Behavioral changes, especially in response to a
+                            particular person
+                          </li>
+                        </ul>
+                        <p>
+                          With my nearly 25 years of experience in a{" "}
+                          <a
+                            href="../other-practice-areas/index.htm"
+                            data-wpel-link="internal"
+                          >
+                            wide range of legal areas
+                          </a>{" "}
+                          and personal commitment to community, you can breathe
+                          a little easier knowing I am representing you.
+                        </p>
+                        <h2>Protect Your Loved One</h2>
+                        <p>
+                          I understand what both insurance companies and nursing
+                          homes will do and say in a lawsuit because I was once
+                          an insurance adjuster. I use this knowledge to create
+                          a strong case and fight for you, and I will keep you
+                          informed at every step of your case. To speak with me,
+                          call my office at{" "}
+                          <span className="nap-item nap-item--phone">
+                            <a
+                              href="tel:+1-404-865-1952"
+                              data-wpel-link="internal"
+                            >
+                              404-865-1952
+                            </a>
+                          </span>{" "}
+                          or{" "}
+                          <a
+                            href="../contact/index.htm"
+                            data-wpel-link="internal"
+                          >
+                            email me
+                          </a>
+                          . I am fluent in English, Spanish and French.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="et_pb_column et_pb_column_1_3 et_pb_column_3_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child">
+                      <div
+                        id="nav-practice-silo"
+                        className="et_pb_module et_pb_sidebar_0_tb_body nav-practice-silo et_pb_widget_area clearfix et_pb_widget_area_left et_pb_bg_layout_light"
+                      >
+                        <div
+                          id="nav_menu-2"
+                          className="et_pb_widget widget_nav_menu"
+                        >
+                          <h2 className="widgettitle">Practice Areas</h2>
+                          <div className="menu-practice-areas-container">
+                            <ul id="menu-practice-areas" className="menu">
+                              <li className="menu-item current-menu-item menu-item--depth-0">
+                                <a
+                                  href="index.htm"
+                                  aria-current="page"
+                                  tabIndex={0}
+                                  data-wpel-link="internal"
+                                >
+                                  Nursing Home Abuse
+                                </a>
+                              </li>
+                              <li className="menu-item menu-item-has-children menu-item--depth-0">
+                                <a
+                                  href="../other-practice-areas/index.htm"
+                                  tabIndex={0}
+                                  data-wpel-link="internal"
+                                >
+                                  Other Practice Areas
+                                </a>
+                                <ul className="sub-menu">
+                                  <li className="menu-item menu-item--depth-1">
+                                    <a
+                                      href="../business-general-counsel-business-transactions/index.htm"
+                                      tabIndex={0}
+                                      data-wpel-link="internal"
+                                    >
+                                      Business General Counsel/ Business
+                                      Transactions
+                                    </a>
+                                  </li>
+                                  <li className="menu-item menu-item--depth-1">
+                                    <a
+                                      href="../criminal-law-defense/index.htm"
+                                      tabIndex={0}
+                                      data-wpel-link="internal"
+                                    >
+                                      Criminal Law Defense
+                                    </a>
+                                  </li>
+                                  <li className="menu-item menu-item--depth-1">
+                                    <a
+                                      href="../serious-injury/index.htm"
+                                      tabIndex={0}
+                                      data-wpel-link="internal"
+                                    >
+                                      Serious Injury
+                                    </a>
+                                  </li>
+                                </ul>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
