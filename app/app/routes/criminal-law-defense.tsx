1830e1554151f27bd437513b1847f95902dcb998
Patrick Howard <patrick.howard@noclocks.dev>
2024-04-09 16:57:38 -0400

    `CriminalLawDefense` page

---
+++ app/app/routes/criminal-law-defense.tsx
@@ -0,0 +1,221 @@
+import {Footer} from '../components/Footer';
+import {Header} from '../components/Header';
+
+import '../styles/criminal-law-defense.css';
+
+export default function CriminalLawDefense() {
+  return (
+    <div id='page-container'>
+      <div
+        id='et-boc'
+        className='et-boc'
+      >
+        <Header />
+        <div id='et-main-area'>
+          <div id='main-content'>
+            <div className='et-l et-l--body'>
+              <div className='et_builder_inner_content et_pb_gutters1'>
+                <div className='et_pb_section et_pb_section_1_tb_body et_pb_with_background et_section_regular'>
+                  <div className='et_pb_row et_pb_row_0_tb_body'>
+                    <div className='et_pb_column et_pb_column_4_4 et_pb_column_0_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child'>
+                      <div className='et_pb_module et_pb_text et_pb_text_0_tb_body  et_pb_text_align_center et_pb_bg_layout_light'>
+                        <div className='et_pb_text_inner'>
+                          <p>
+                            Dedication. Skill. Innovation.
+                            <br /> Trust The{' '}
+                            <strong>
+                              <span className='nap-item nap-item--name'>
+                                Law Offices of Villard Bastien, LLC
+                              </span>
+                            </strong>
+                            .
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='et_pb_section et_pb_section_3_tb_body fl-breadcrumbs et_pb_with_background et_section_regular'>
+                  <div className='et_pb_row et_pb_row_1_tb_body'>
+                    <div className='et_pb_column et_pb_column_4_4 et_pb_column_1_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child'>
+                      <div className='et_pb_module et_pb_dcsbcm_divi_breadcrumbs_module et_pb_dcsbcm_divi_breadcrumbs_module_0_tb_body'>
+                        <div className='et_pb_module_inner'>
+                          <ol
+                            className='dcsbcm_divi_breadcrumbs'
+                            // vocab='http://schema.org/'
+                            // typeof='BreadcrumbList'
+                          >
+                            <li
+                              className='dcsbcm_divi_breadcrumb'
+                              // property='itemListElement'
+                              // typeof='ListItem'
+                            >
+                              <a
+                                // property='item'
+                                // typeof='WebPage'
+                                href='/'
+                              >
+                                <span
+                                  // property='name'
+                                >
+                                  Home
+                                </span>
+                              </a>
+                              <meta
+                                property='position'
+                                content='1'
+                              />
+                            </li>
+                            <span className='dcsbcm_separator'>
+                              &nbsp;&raquo;&nbsp;
+                            </span>
+                            <span className='dcsbcm_divi_breadcrumb dcsbcm_divi_breadcrumb-active'>
+                              Criminal Law Defense
+                            </span>
+                          </ol>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='et_pb_section et_pb_section_4_tb_body et_pb_with_background et_section_regular'>
+                  <div className='et_pb_row et_pb_row_2_tb_body'>
+                    <div className='et_pb_column et_pb_column_2_3 et_pb_column_2_tb_body fl-main-content  et_pb_css_mix_blend_mode_passthrough'>
+                      <div className='et_pb_module et_pb_post_content et_pb_post_content_0_tb_body'>
+                        <h1>Years of Criminal Law Experience</h1>
+                        <p>
+                          Attorney Bastien has been exposed to criminal law
+                          since the early nineties. He began his exposure by
+                          interning summers and school semesters with the Queens
+                          District Attorney&rsquo;s Office. Since that beginning
+                          Criminal Law and Criminal defense has been part of his
+                          practice. He handles the following matters in criminal
+                          law;
+                        </p>
+                        <ul>
+                          <li>DUI</li>
+                          <li>Traffic Infractions</li>
+                          <li>Battery</li>
+                          <li>Aggravated Assault</li>
+                          <li>Fraud Allegations</li>
+                          <li>Shoplifting</li>
+                          <li>DFCS/ Allegation against Children</li>
+                        </ul>
+                        <p>
+                          He takes the time to listen to your version of the
+                          facts. He is not looking to push your case quickly and
+                          force you to plea to something that is not beneficial
+                          to you. Attorney Bastien, will make sure he gets the
+                          discovery needed to properly evaluate your case. Make
+                          the motions needed to force the prosecution to divulge
+                          the flaws and assumptions in your case. If a plea is
+                          in your best interest, he will make sure you get the
+                          best negotiations available to you. He is not afraid
+                          to go to trial if necessary. He has tried many
+                          criminal cases to verdict or judgment resulting in
+                          dismissals. For example, In DUI cases he will make
+                          sure that the officers and prosecutors acted properly
+                          or have the evidence claimed. He will force them to
+                          prove every portion of their case. On other criminal
+                          cases he breaks down the statute and makes sure that
+                          each and every portion of the criminal statute can be
+                          proven. He also makes sure that any evidence secured
+                          was properly and lawfully obtained. He will challenge
+                          any illegal or inappropriate search seizure or
+                          obtained evidence.
+                        </p>
+                        <p>
+                          Do not wait. Call me now at{' '}
+                          <span className='nap-item nap-item--phone'>
+                            <a
+                              href='tel:+1-404-865-1952'
+                              data-wpel-link='internal'
+                            >
+                              404-865-1952
+                            </a>
+                          </span>{' '}
+                          for a free consultation. I speak English, Spanish and
+                          French.
+                        </p>
+                      </div>
+                    </div>
+                    <div className='et_pb_column et_pb_column_1_3 et_pb_column_3_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child'>
+                      <div
+                        id='nav-practice-silo'
+                        className='et_pb_module et_pb_sidebar_0_tb_body nav-practice-silo et_pb_widget_area clearfix et_pb_widget_area_left et_pb_bg_layout_light'
+                      >
+                        <div
+                          id='nav_menu-2'
+                          className='et_pb_widget widget_nav_menu'
+                        >
+                          <h2 className='widgettitle'>Practice Areas</h2>
+                          <div className='menu-practice-areas-container'>
+                            <ul
+                              id='menu-practice-areas'
+                              className='menu'
+                            >
+                              <li className='menu-item menu-item--depth-0'>
+                                <a
+                                  href='../nursing-home-abuse/index.htm'
+                                  tabIndex={0}
+                                  data-wpel-link='internal'
+                                >
+                                  Nursing Home Abuse
+                                </a>
+                              </li>
+                              <li className='menu-item menu-item-has-children current-menu-parent menu-item--depth-0'>
+                                <a
+                                  href='../other-practice-areas/index.htm'
+                                  tabIndex={0}
+                                  data-wpel-link='internal'
+                                >
+                                  Other Practice Areas
+                                </a>
+                                <ul className='sub-menu'>
+                                  <li className='menu-item menu-item--depth-1'>
+                                    <a
+                                      href='../business-general-counsel-business-transactions/index.htm'
+                                      tabIndex={0}
+                                      data-wpel-link='internal'
+                                    >
+                                      Business General Counsel/ Business
+                                      Transactions
+                                    </a>
+                                  </li>
+                                  <li className='menu-item current-menu-item menu-item--depth-1'>
+                                    <a
+                                      href='index.htm'
+                                      aria-current='page'
+                                      tabIndex={0}
+                                      data-wpel-link='internal'
+                                    >
+                                      Criminal Law Defense
+                                    </a>
+                                  </li>
+                                  <li className='menu-item menu-item--depth-1'>
+                                    <a
+                                      href='../serious-injury/index.htm'
+                                      tabIndex={0}
+                                      data-wpel-link='internal'
+                                    >
+                                      Serious Injury
+                                    </a>
+                                  </li>
+                                </ul>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
