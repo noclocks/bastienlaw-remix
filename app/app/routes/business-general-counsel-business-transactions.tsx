98ba988cffefe7fa1c4944a57ec15687476c3c05
Patrick Howard <patrick.howard@noclocks.dev>
2024-04-09 16:47:35 -0400

    `BusinessGeneralCounselBusinessTransactions` page

---
+++ app/app/routes/business-general-counsel-business-transactions.tsx
@@ -0,0 +1,212 @@
+import {Footer} from '../components/Footer';
+import {Header} from '../components/Header';
+
+import '../styles/business-general-counsel-business-transactions.css';
+
+export default function BusinessGeneralCounselBusinessTransactions() {
+  return (
+    <div id='page-container'>
+      <div
+        id='et-boc'
+        className='et-boc'
+      >
+        <Header />
+        <div id='et-main-area'>
+          <div id='main-content'>
+            <div className='et-l et-l--body'>
+              <div className='et_builder_inner_content et_pb_gutters1'>
+                <div className='et_pb_section et_pb_section_1_tb_body et_pb_with_background et_section_regular'>
+                  <div className='et_pb_row et_pb_row_0_tb_body'>
+                    <div className='et_pb_column et_pb_column_4_4 et_pb_column_0_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child'>
+                      <div className='et_pb_module et_pb_text et_pb_text_0_tb_body  et_pb_text_align_center et_pb_bg_layout_light'>
+                        <div className='et_pb_text_inner'>
+                          <p>
+                            Dedication. Skill. Innovation.
+                            <br /> Trust The{' '}
+                            <strong>
+                              <span className='nap-item nap-item--name'>
+                                Law Offices of Villard Bastien, LLC
+                              </span>
+                            </strong>
+                            .
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='et_pb_section et_pb_section_3_tb_body fl-breadcrumbs et_pb_with_background et_section_regular'>
+                  <div className='et_pb_row et_pb_row_1_tb_body'>
+                    <div className='et_pb_column et_pb_column_4_4 et_pb_column_1_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child'>
+                      <div className='et_pb_module et_pb_dcsbcm_divi_breadcrumbs_module et_pb_dcsbcm_divi_breadcrumbs_module_0_tb_body'>
+                        <div className='et_pb_module_inner'>
+                          <ol
+                            className='dcsbcm_divi_breadcrumbs'
+                            // vocab="http://schema.org/"
+                            // typeof="BreadcrumbList"
+                          >
+                            <li
+                              className='dcsbcm_divi_breadcrumb'
+                              // property="itemListElement"
+                              // typeof="ListItem"
+                            >
+                              <a
+                                // property="item"
+                                // typeof="WebPage"
+                                href='../index.htm'
+                              >
+                                <span
+                                // property="name"
+                                >
+                                  Home
+                                </span>
+                              </a>
+                              <meta
+                                property='position'
+                                content='1'
+                              />
+                            </li>
+                            <span className='dcsbcm_separator'>
+                              &nbsp;&raquo;&nbsp;
+                            </span>
+                            <span className='dcsbcm_divi_breadcrumb dcsbcm_divi_breadcrumb-active'>
+                              Business General Counsel / Business Transactions
+                            </span>
+                          </ol>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='et_pb_section et_pb_section_4_tb_body et_pb_with_background et_section_regular'>
+                  <div className='et_pb_row et_pb_row_2_tb_body'>
+                    <div className='et_pb_column et_pb_column_2_3 et_pb_column_2_tb_body fl-main-content  et_pb_css_mix_blend_mode_passthrough'>
+                      <div className='et_pb_module et_pb_post_content et_pb_post_content_0_tb_body'>
+                        <h1>
+                          Helping You Through Your Business Transactions Needs
+                        </h1>
+                        <p>
+                          Attorney Bastien sets himself apart from other
+                          Business Attorneys in that he enjoys being a business
+                          professional just as much as practicing law. He has
+                          always balanced his passion for the business world
+                          with that of the law.
+                        </p>
+                        <p>
+                          After earning his law degree, he furthered his
+                          education by specializing in Business Practice by
+                          earning a Master&rsquo;s of Law (LL.M.). He also
+                          worked for a corporation prior to actively practicing
+                          law. He actively evaluates and counsel&rsquo;s
+                          businesses with the help of other business
+                          professionals such as accountants and certified
+                          financial planners. Attorney Bastien has also had the
+                          advantage of being exposed to varied facets of the
+                          law, such as criminal law, torts litigation, contracts
+                          litigation, thus enabling him the ability to be able
+                          to effectively counsel your business for its needs.
+                        </p>
+                        <p>
+                          Attorney Bastien has also amassed international
+                          experience should you wish to expand your business
+                          globally. It also helps that he is fluent in Spanish,
+                          French and has a working knowledge of Italian.
+                        </p>
+                        <p>
+                          Attorney Bastien can satisfy the day-to-day needs of
+                          your business, so you can rest well knowing you always
+                          have an Attorney you can count on. Having consistent
+                          legal counsel frees up your time and energy to focus
+                          on growing your enterprise.
+                        </p>
+                        <p>
+                          With extensive knowledge and real-world business
+                          experience, Attorney Bastien can guide your company to
+                          be stable and efficient. Attorney Bastien will help
+                          you evaluate all needs of the company.
+                        </p>
+                        <p>
+                          Do not wait. Call me now at{' '}
+                          <span className='nap-item nap-item--phone'>
+                            <a href='tel:+1-404-865-1952'>404-865-1952</a>
+                          </span>{' '}
+                          for a free consultation. I speak English, Spanish and
+                          French.
+                        </p>
+                      </div>
+                    </div>
+                    <div className='et_pb_column et_pb_column_1_3 et_pb_column_3_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child'>
+                      <div
+                        id='nav-practice-silo'
+                        className='et_pb_module et_pb_sidebar_0_tb_body nav-practice-silo et_pb_widget_area clearfix et_pb_widget_area_left et_pb_bg_layout_light'
+                      >
+                        <div
+                          id='nav_menu-2'
+                          className='et_pb_widget widget_nav_menu'
+                        >
+                          <h2 className='widgettitle'>Practice Areas</h2>
+                          <div className='menu-practice-areas-container'>
+                            <ul
+                              id='menu-practice-areas'
+                              className='menu'
+                            >
+                              <li className='menu-item menu-item--depth-0'>
+                                <a
+                                  href='/nursing-home-abuse'
+                                  tabIndex={0}
+                                >
+                                  Nursing Home Abuse
+                                </a>
+                              </li>
+                              <li className='menu-item menu-item-has-children current-menu-parent menu-item--depth-0'>
+                                <a
+                                  href='../other-practice-areas/index.htm'
+                                  tabIndex={0}
+                                >
+                                  Other Practice Areas
+                                </a>
+                                <ul className='sub-menu'>
+                                  <li className='menu-item current-menu-item menu-item--depth-1'>
+                                    <a
+                                      href='index.htm'
+                                      aria-current='page'
+                                      tabIndex={0}
+                                    >
+                                      Business General Counsel/ Business
+                                      Transactions
+                                    </a>
+                                  </li>
+                                  <li className='menu-item menu-item--depth-1'>
+                                    <a
+                                      href='../criminal-law-defense/index.htm'
+                                      tabIndex={0}
+                                    >
+                                      Criminal Law Defense
+                                    </a>
+                                  </li>
+                                  <li className='menu-item menu-item--depth-1'>
+                                    <a
+                                      href='../serious-injury/index.htm'
+                                      tabIndex={0}
+                                    >
+                                      Serious Injury
+                                    </a>
+                                  </li>
+                                </ul>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
