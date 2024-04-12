898e0936ecdeec5dbf55bfa58315449287d4bd27
Patrick Howard <patrick.howard@noclocks.dev>
2024-04-12 14:25:30 -0400

    `AtlantaMedicalMalpracticeLawOffice` page

---
+++ app/app/routes/atlanta-medical-malpractice-law-office.tsx
@@ -0,0 +1,193 @@
+import {Footer} from '../components/Footer';
+import {Header} from '../components/Header';
+
+import '../styles/nursing-home-abuse.css';
+
+export default function AtlantaMedicalMalpracticeLawOffice() {
+  return (
+    <div id='page-container'>
+      <div
+        id='et-boc'
+        className='et-boc'
+      >
+        <Header />
+        <div id='et-main-area'>
+          <div id='main-content'>
+            <div className='et-l et-l--body'>
+              <div className='et_builder_inner_content et_pb_gutters1'>
+                <div className='et_pb_section et_pb_section_1_tb_body et_pb_with_background et_section_regular'>
+                  <div className='et_pb_row et_pb_row_0_tb_body'>
+                    <div className='et_pb_column et_pb_column_4_4 et_pb_column_0_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child'>
+                      <div className='et_pb_module et_pb_text et_pb_text_0_tb_body  et_pb_text_align_center et_pb_bg_layout_light'>
+                        <div className='et_pb_text_inner'>
+                          <p>
+                            Dedication. Skill. Innovation.
+                            <br /> Trust The{' '}
+                            <strong>
+                              <span className='nap-item nap-item--name'>
+                                Law Offices of Villard Bastien, LLC
+                              </span>
+                            </strong>
+                            .
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='et_pb_section et_pb_section_3_tb_body fl-breadcrumbs et_pb_with_background et_section_regular'>
+                  <div className='et_pb_row et_pb_row_1_tb_body'>
+                    <div className='et_pb_column et_pb_column_4_4 et_pb_column_1_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child'>
+                      <div className='et_pb_module et_pb_dcsbcm_divi_breadcrumbs_module et_pb_dcsbcm_divi_breadcrumbs_module_0_tb_body'>
+                        <div className='et_pb_module_inner'>
+                          <ol className='dcsbcm_divi_breadcrumbs'>
+                            <li className='dcsbcm_divi_breadcrumb'>
+                              <a href='../index.htm'>
+                                <span>Home</span>
+                              </a>
+                              <meta
+                                property='position'
+                                content='1'
+                              />
+                            </li>
+                            <span className='dcsbcm_separator'>
+                              &nbsp;&raquo;&nbsp;
+                            </span>
+                            <span className='dcsbcm_divi_breadcrumb dcsbcm_divi_breadcrumb-active'>
+                              Atlanta Medical Malpractice Law Office
+                            </span>
+                          </ol>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='et_pb_section et_pb_section_4_tb_body et_pb_with_background et_section_regular'>
+                  <div className='et_pb_row et_pb_row_2_tb_body'>
+                    <div className='et_pb_column et_pb_column_2_3 et_pb_column_2_tb_body fl-main-content  et_pb_css_mix_blend_mode_passthrough'>
+                      <div className='et_pb_module et_pb_post_content et_pb_post_content_0_tb_body'>
+                        <h1>Atlanta Medical Malpractice Law Office</h1>
+                        <p>
+                          <strong>
+                            <span className='nap-item nap-item--name'>
+                              Law Offices of Villard Bastien, LLC
+                            </span>
+                          </strong>
+                        </p>
+                        <p>
+                          <span className='nap-item nap-item--address'>
+                            200 Ashford Center N<br />
+                            Ste 215
+                            <br />
+                            Atlanta, GA 30338
+                          </span>
+                          <br />
+                          <strong>Telephone:</strong>{' '}
+                          <span className='nap-item nap-item--phone'>
+                            <a
+                              href='tel:+1-404-865-1952'
+                              data-wpel-link='internal'
+                            >
+                              404-865-1952
+                            </a>
+                          </span>
+                        </p>
+                        <p>
+                          <a
+                            href='https://maps.app.goo.gl/NhoZ4GSYWEx98mAP7'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                          >
+                            Atlanta Office
+                          </a>
+                        </p>
+                        <p>
+                          <iframe
+                            title='Atlanta Office Map'
+                            loading='lazy'
+                            style={{border: 0}}
+                            src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13239.774398262407!2d-84.3362101!3d33.9425787!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x939dfd38e665bcbd!2sLaw%20Offices%20of%20Villard%20Bastien%2C%20LLC!5e0!3m2!1sen!2sus!4v1624636476233!5m2!1sen!2sus'
+                            width='600'
+                            height='450'
+                            allowFullScreen={true}
+                            data-src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13239.774398262407!2d-84.3362101!3d33.9425787!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x939dfd38e665bcbd!2sLaw%20Offices%20of%20Villard%20Bastien%2C%20LLC!5e0!3m2!1sen!2sus!4v1624636476233!5m2!1sen!2sus'
+                            className=' tr-jch-lazyload'
+                          ></iframe>
+                        </p>
+                      </div>
+                    </div>
+                    <div className='et_pb_column et_pb_column_1_3 et_pb_column_3_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child'>
+                      <div
+                        id='nav-practice-silo'
+                        className='et_pb_module et_pb_sidebar_0_tb_body nav-practice-silo et_pb_widget_area clearfix et_pb_widget_area_left et_pb_bg_layout_light'
+                      >
+                        <div
+                          id='nav_menu-2'
+                          className='et_pb_widget widget_nav_menu'
+                        >
+                          <h2 className='widgettitle'>Practice Areas</h2>
+                          <div className='menu-practice-areas-container'>
+                            <ul
+                              id='menu-practice-areas'
+                              className='menu'
+                            >
+                              <li className='menu-item current-menu-item menu-item--depth-0'>
+                                <a
+                                  href='index.htm'
+                                  aria-current='page'
+                                  tabIndex={0}
+                                >
+                                  Nursing Home Abuse
+                                </a>
+                              </li>
+                              <li className='menu-item menu-item-has-children menu-item--depth-0'>
+                                <a
+                                  href='../other-practice-areas/index.htm'
+                                  tabIndex={0}
+                                >
+                                  Other Practice Areas
+                                </a>
+                                <ul className='sub-menu'>
+                                  <li className='menu-item menu-item--depth-1'>
+                                    <a
+                                      href='../business-general-counsel-business-transactions/index.htm'
+                                      tabIndex={0}
+                                    >
+                                      Business General Counsel/ Business
+                                      Transactions
+                                    </a>
+                                  </li>
+                                  <li className='menu-item menu-item--depth-1'>
+                                    <a
+                                      href='../criminal-law-defense/index.htm'
+                                      tabIndex={0}
+                                    >
+                                      Criminal Law Defense
+                                    </a>
+                                  </li>
+                                  <li className='menu-item menu-item--depth-1'>
+                                    <a
+                                      href='../serious-injury/index.htm'
+                                      tabIndex={0}
+                                    >
+                                      Serious Injury
+                                    </a>
+                                  </li>
+                                </ul>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
