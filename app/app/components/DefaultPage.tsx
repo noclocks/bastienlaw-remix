79db69ff520791acd9d2f8f51cdd27c8e369ea2b
Patrick Howard <patrick.howard@noclocks.dev>
2024-04-16 16:18:32 -0400

    new `DefaultPage` component

---
+++ app/app/components/DefaultPage.tsx
@@ -0,0 +1,160 @@
+import {Header} from '../components/Header';
+import {Footer} from '../components/Footer';
+
+import '../styles/nursing-home-abuse.css';
+
+export interface DefaultPageProps {
+  title: string;
+  description: string | JSX.Element;
+  includeSidebar?: boolean;
+}
+
+export default function DefaultPage(props: DefaultPageProps) {
+  return (
+    <div id='page-container'>
+      <div
+        id='et-boc'
+        className='et-boc'
+      >
+        <Header />
+        <div id='et-main-area'>
+          <div id='main-content'>
+            <div className='et-l et-l--body'>
+              <div className='et_builder_inner_content et_pb_gutters1'>
+                <div className='et_pb_section et_pb_section_1_tb_body et_pb_with_background et_section_regular'>
+                  <div className='et_pb_row et_pb_row_0_tb_body'>
+                    <div className='et_pb_column et_pb_column_4_4 et_pb_column_0_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child'>
+                      <div className='et_pb_module et_pb_text et_pb_text_0_tb_body  et_pb_text_align_center et_pb_bg_layout_light'>
+                        <div className='et_pb_text_inner'>
+                          <p>
+                            Dedication. Skill. Innovation.
+                            <br /> Trust The{' '}
+                            <strong>
+                              <span className='nap-item nap-item--name'>
+                                Law Offices of Villard Bastien, LLC
+                              </span>
+                            </strong>
+                            .
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='et_pb_section et_pb_section_3_tb_body fl-breadcrumbs et_pb_with_background et_section_regular'>
+                  <div className='et_pb_row et_pb_row_1_tb_body'>
+                    <div className='et_pb_column et_pb_column_4_4 et_pb_column_1_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child'>
+                      <div className='et_pb_module et_pb_dcsbcm_divi_breadcrumbs_module et_pb_dcsbcm_divi_breadcrumbs_module_0_tb_body'>
+                        <div className='et_pb_module_inner'>
+                          <ol className='dcsbcm_divi_breadcrumbs'>
+                            <li className='dcsbcm_divi_breadcrumb'>
+                              <a href='/'>
+                                <span>Home</span>
+                              </a>
+                              <meta
+                                property='position'
+                                content='1'
+                              />
+                            </li>
+                            <span className='dcsbcm_separator'>
+                              &nbsp;&raquo;&nbsp;
+                            </span>
+                            <span className='dcsbcm_divi_breadcrumb dcsbcm_divi_breadcrumb-active'>
+                              {props.title}
+                            </span>
+                          </ol>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='et_pb_section et_pb_section_4_tb_body et_pb_with_background et_section_regular'>
+                  <div className='et_pb_row et_pb_row_2_tb_body'>
+                    <div
+                      className={
+                        !props.includeSidebar
+                          ? 'et_pb_column et_pb_column_2_tb_body fl-main-content  et_pb_css_mix_blend_mode_passthrough'
+                          : 'et_pb_column et_pb_column_2_3 et_pb_column_2_tb_body fl-main-content  et_pb_css_mix_blend_mode_passthrough'
+                      }
+                    >
+                      <div className='et_pb_module et_pb_post_content et_pb_post_content_0_tb_body'>
+                        <h1>{props.title}</h1>
+                        <p>{props.description}</p>
+                      </div>
+                    </div>
+                    {!props.includeSidebar ? null : (
+                      <div className='et_pb_column et_pb_column_1_3 et_pb_column_3_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child'>
+                        <div
+                          id='nav-practice-silo'
+                          className='et_pb_module et_pb_sidebar_0_tb_body nav-practice-silo et_pb_widget_area clearfix et_pb_widget_area_left et_pb_bg_layout_light'
+                        >
+                          <div
+                            id='nav_menu-2'
+                            className='et_pb_widget widget_nav_menu'
+                          >
+                            <h2 className='widgettitle'>Practice Areas</h2>
+                            <div className='menu-practice-areas-container'>
+                              <ul
+                                id='menu-practice-areas'
+                                className='menu'
+                              >
+                                <li className='menu-item current-menu-item menu-item--depth-0'>
+                                  <a
+                                    href='nursing-home-abuse'
+                                    aria-current='page'
+                                    tabIndex={0}
+                                  >
+                                    Nursing Home Abuse
+                                  </a>
+                                </li>
+                                <li className='menu-item menu-item-has-children menu-item--depth-0'>
+                                  <a
+                                    href='other-practice-areas'
+                                    tabIndex={0}
+                                  >
+                                    Other Practice Areas
+                                  </a>
+                                  <ul className='sub-menu'>
+                                    <li className='menu-item menu-item--depth-1'>
+                                      <a
+                                        href='business-general-counsel-business-transactions'
+                                        tabIndex={0}
+                                      >
+                                        Business General Counsel / Business Transactions
+                                      </a>
+                                    </li>
+                                    <li className='menu-item menu-item--depth-1'>
+                                      <a
+                                        href='criminal-law-defense'
+                                        tabIndex={0}
+                                      >
+                                        Criminal Law Defense
+                                      </a>
+                                    </li>
+                                    <li className='menu-item menu-item--depth-1'>
+                                      <a
+                                        href='serious-injury'
+                                        tabIndex={0}
+                                      >
+                                        Serious Injury
+                                      </a>
+                                    </li>
+                                  </ul>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
