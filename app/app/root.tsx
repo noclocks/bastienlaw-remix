83da898f031ea80a3afcccd2e667134e8095201f
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2024-10-03 14:28:22 -0400

    feat: add gtags support

--- app/app/root.tsx
+++ app/app/root.tsx
@@ -1,19 +1,31 @@
+import { json } from "@remix-run/node";
+
 import {
+  Link,
   Links,
   Meta,
   MetaFunction,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useLocation
 } from "@remix-run/react";
+
+import { useEffect } from "react";
+
+import * as gtag from "./utils/gtags.client";
 
 import "./styles/typography.css";
 import './styles/global.css';
-import ScrollToTopButton from "./components/ScrollToTopButton";
-// import "./styles/dsethtml.css"
-// import "./styles/dsethtml2.css"
 
+import ScrollToTopButton from "./components/ScrollToTopButton";
 // import ChatbotScript from "./components/ChatBot";
+
+// Load the GA tracking id from the .env
+export const loader = async () => {
+  return json({ gaTrackingId: "G-BQBBSBFGZG" });
+};
 
 export const meta: MetaFunction = () => {
   return [
@@ -22,7 +34,16 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export function Layout({children}: {children: React.ReactNode}) {
+export function Layout({ children }: { children: React.ReactNode }) {
+  const location = useLocation();
+  const gaTrackingId = "G-BQBBSBFGZG";
+
+  useEffect(() => {
+    if (gaTrackingId?.length) {
+      gtag.pageview(location.pathname, gaTrackingId);
+    }
+  }, [location, gaTrackingId]);
+
   return (
     <html lang="en">
       <head>
@@ -32,6 +53,29 @@ export function Layout({children}: {children: React.ReactNode}) {
         <Links />
       </head>
       <body>
+        {process.env.NODE_ENV === "development" || !gaTrackingId ? null : (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            />
+            <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+            />
+          </>
+        )}
         {[children, <ScrollToTopButton key={0} />]}
         <ScrollRestoration />
         <Scripts />
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,15 +4,34 @@ import {defineConfig} from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  // assetsInclude: [
-  //   "assets/**",
-  //   "wp-content/**",
-  //   "app/wp-content/**",
-  //   "wp-content/uploads/sites/1302270/2021/06/**",
-  //   "wp-content/uploads/sites/1302270/2023/09/**",
-  // ],
-  // base: "/",
   appType: "spa",
+  plugins: [
+    remix({
+      ssr: false,
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    remixDevTools({
+      includeInProd: false,
+    }),
+    tsconfigPaths(),
+  ],
+  server: {
+    cors: false,
+    port: 8080,
+    strictPort: true,
+    host: "0.0.0.0",
+    origin: "http://0.0.0.0:3000",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Credentials": "true",
+    },
+  },
   preview: {
     port: 8080,
     strictPort: true,
@@ -24,78 +43,5 @@ export default defineConfig({
       "Access-Control-Allow-Headers": "*",
       "Access-Control-Allow-Credentials": "true",
     },
-  },
-  server: {
-    cors: false,
-    port: 8080,
-    strictPort: true,
-    host: "0.0.0.0",
-    origin: "http://0.0.0.0:8080",
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "*",
-      "Access-Control-Allow-Credentials": "true",
-    },
-    // host: true,
-    // open: true,
-    // watch: {
-    // 	alwaysStat: true,
-    // 	persistent: true,
-    // },
-  },
-  // 	},
-  // },
-  // css: ,
-  // build: {
-  //   watch: {
-  //     include
-  //   },
-  // }
-  // envPrefix: 'APP_',
-  plugins: [
-    remixDevTools({
-      includeInProd: false,
-    }),
-    remix({
-      ssr: false,
-      // ignoredRouteFiles: [
-      //   "app/assets/*",
-      //   "app/wp-content/*",
-      //   "app/wp-content/**",
-      //   "app/google-fonts/*",
-      //   "app/google-fonts/**",
-      //   "app/wp-content/uploads/sites/1302270/2023/09/*",
-      //   "app/wp-content/uploads/sites/1302270/2021/06/*",
-      //   "app/wp-content/index-files/**",
-      //   // "app/wp-content/index-files/cc40ba9a93e87e26f1018e89bcb019711766ec8043887571a4c9e737f74cf36c.css",
-      // ],
-    }),
-    // !process.env.VITEST &&
-    // 	remix({
-    // 		// serverModuleFormat: ''
-    // 		// appDirectory: 'app',
-    // 		// basename: './',
-    // 		ssr: false,
-    // 		ignoredRouteFiles: [
-    // 			'app/assets/*',
-    // 			'app/wp-content/*',
-    // 			'app/wp-content/**',
-    // 			'app/google-fonts/*',
-    // 			'app/google-fonts/**',
-    // 			'app/wp-content/uploads/sites/1302270/2023/09/*',
-    // 			'app/wp-content/uploads/sites/1302270/2021/06/*',
-    // 			'app/wp-content/index-files/**',
-    // 			// "app/wp-content/index-files/cc40ba9a93e87e26f1018e89bcb019711766ec8043887571a4c9e737f74cf36c.css",
-    // 		],
-    // 	}),
-    tsconfigPaths(),
-  ],
-}); //=> {
-// 	const env = loadEnv('', process.cwd(), '');
-// 	return {
-// 		define: {
-// 			__APP_ENV__: JSON.stringify(env.APP_ENV),
-// 		},
-// 	}
-// };
+  }
+});
